a0380f2b8461b71fac74d502ab50beb330973f9b
Laura Vieira <lauramvieira.aus@gmail.com>
2021-02-18 22:54:08 -0300

    feat: get classes by school

--- src/controllers/SchoolController.js
+++ src/controllers/SchoolController.js
@@ -1,8 +1,16 @@
 const School = require('../models/School');
+const Class = require('../models/Class');
 
 class SchoolController {
   getSchoolById (id) {
     return School.findByPk(id);
+  }
+
+  getClassesBySchool (id) {
+    return Class.findAll({
+      where: { schoolId: id },
+      order: [['name']]
+    });
   }
 }
 
