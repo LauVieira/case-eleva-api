ef7ef2aef3dbdd5928407799defe4aaf0ed105eb
Laura Vieira <lauramvieira.aus@gmail.com>
2021-02-18 23:04:26 -0300

    School-Class relationship

--- src/utils/loadRelationships.js
+++ src/utils/loadRelationships.js
@@ -1,5 +1,8 @@
-const Sector = require('../models/Sector');
+const Class = require('../models/Class');
 const School = require('../models/School');
+const Sector = require('../models/Sector');
 
 School.belongsTo(Sector);
 Sector.hasMany(School);
+Class.belongsTo(School);
+School.hasMany(Class);
