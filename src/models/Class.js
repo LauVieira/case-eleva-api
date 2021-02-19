242fbef3e4da35c6aa25b0449b6750d60cb468ce
Laura Vieira <lauramvieira.aus@gmail.com>
2021-02-18 21:59:38 -0300

    feat: Class Model

---
+++ src/models/Class.js
@@ -0,0 +1,49 @@
+const { Sequelize } = require('sequelize');
+const sequelize = require('../utils/database');
+
+class Class extends Sequelize.Model { }
+
+Class.init(
+  {
+    id: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true,
+    },
+    schoolId: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+    },
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    grade: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    segment: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    shift: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    status: {
+      type: Sequelize.ENUM('actual', 'completed'),
+      allowNull: false,
+    },
+    maxOccupancy: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+    },
+  }, 
+  {
+    sequelize,
+    modelName: 'class',
+  }
+);
+
+module.exports = Class;
